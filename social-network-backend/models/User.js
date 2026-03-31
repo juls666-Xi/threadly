610f094e02a248ee8e9155ecdb6855e68d285c4c
juls666-Xi <sfxcs.juliusjordan@gmail.com>
2026-03-31 05:55:05 +0000

    aa

--- social-network-backend/models/User.js
+++ social-network-backend/models/User.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
+  name: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
     minlength: 3,
     maxlength: 20
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true
   },
   password: {
     type: String,
